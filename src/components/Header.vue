36915f906180bbf67db314e002a5a1d41e22c6cf
Григорьев Ростислав <43699715+Irlirion@users.noreply.github.com>
2020-05-27 20:08:52 +0400

    fix for heroku

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
     <header>
         <b-navbar toggleable="sm" type="light" variant="light">
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-            <b-navbar-brand to="/vue_website/">Григорьев Ростислав</b-navbar-brand>
+            <b-navbar-brand to="/">Григорьев Ростислав</b-navbar-brand>
             <b-collapse id="nav-collapse" is-nav>
                 <b-navbar-nav class="ml-auto">
                     <b-nav-item to="/calculator">Калькулятор</b-nav-item>
--- src/router.js
+++ src/router.js
@@ -7,7 +7,7 @@ Vue.use(Router);
 export default new Router({
     mode: 'history',
     routes: [
-        {path: '/vue_website/', component: Home},
+        {path: '/', component: Home},
         {path: '/about', component: () => import('./views/About.vue')},
         {path: '/calculator', component: () => import('./views/Calculator.vue')},
         {path: '/test', component: () => import('./views/Tests.vue')},
